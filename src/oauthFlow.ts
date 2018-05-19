bccd9228b184e6b6a01ee98c9aee98d1660cf9df
Pedro Barquinha <barquinha.pedro@gmail.com>
2018-05-19 18:25:55 -0400

    added menu graphs list

--- src/oauthFlow.ts
+++ src/oauthFlow.ts
@@ -9,12 +9,11 @@ interface User {
 var user: User = {id: '', name: '', link: ''};
 
 window.onload = function() {
-  document.querySelector('button').addEventListener('click', function() {
-    chrome.identity.getAuthToken({interactive: true}, function(token) {
+  chrome.identity.getAuthToken({interactive: true}, function(token) {
     axios.get('https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=' + token)
     .then((response: any) => {
       const { id, name, link } = response.data;
-
+      
       axios.post('http:localhost:3005/api/chromeSession', { id })
       .then((res: any) => {
         console.log('session:', res);
@@ -24,8 +23,7 @@ window.onload = function() {
     .catch((err: any) => {
       console.log(err);
     })
-    });  
-  });
+  });  
 };
 
 export default user;
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,8 @@ module.exports = {
         app: path.join(__dirname, "src/index.tsx"),
         eventPage: path.join(__dirname, "src/eventPage.tsx"),
         auth: path.join(__dirname, "src/auth.ts"),
-        oauthFlow: path.join(__dirname, "src/oauthFlow.ts")
+        oauthFlow: path.join(__dirname, "src/oauthFlow.ts"),
+        menu: path.join(__dirname, "src/Menu.tsx")
     },
     devtool: "inline-source-map",
     output: {
@@ -32,7 +33,7 @@ module.exports = {
                 ],
               }
         ]
-    },
+        },
     resolve: {
         extensions: [".ts", ".tsx", ".js"]
     }
